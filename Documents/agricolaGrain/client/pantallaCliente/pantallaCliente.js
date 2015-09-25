aed871ed3cbaef2347a199ec34b81c477cde3592
unknown <edwinfernando22@gmail.com>
2015-09-25 15:35:43 -0600

    Pantalla Cliente

--- Documents/agricolaGrain/client/pantallaCliente/pantallaCliente.js
+++ Documents/agricolaGrain/client/pantallaCliente/pantallaCliente.js
@@ -1,4 +1,22 @@
 Template.pantallaCliente.onRendered(function(){
 $('ul.tabs').tabs();
 $('select').material_select();
+$("#solicitudesPendientesU").show("slow");
+});
+Template.pantallaCliente.events({
+"click #btnPendientesU":function(event,template){
+	$("#solicitudesPendientesU").show("slow");
+	$("#solicitudesProcesoU").hide("slow");
+	$("#solicitudesFinalizadasU").hide("slow");
+},
+"click #btnProcesoU":function(event,template){
+	$("#solicitudesPendientesU").hide("slow");
+	$("#solicitudesProcesoU").show("slow");
+	$("#solicitudesFinalizadasU").hide("slow");
+},
+"click #btnFinalizadasU":function(event,template){
+	$("#solicitudesPendientesU").hide("slow");
+	$("#solicitudesProcesoU").hide("slow");
+	$("#solicitudesFinalizadasU").show("slow");
+}
 });
